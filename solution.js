ded0eb096b9777b3a0768f7c03d913c14f2579be
RanaMuhammadIrshad <rmirshad@web.de>
2022-06-20 10:19:33 +0200

    some changes in variable names

--- solution.js
+++ solution.js
@@ -1,174 +1,175 @@
-// "use strict";
+"use strict";
 
-// /*
-// **1. Check if a number is within a given range.**
-// Create a function `isWithinRange` that checks if a number is within the range of an object's min and max properties. Assume min <= max is always true.
+/*
+**1. Check if a number is within a given range.**
+Create a function `isWithinRange` that checks if a number is within the range of an object's min and max properties. Assume min <= max is always true.
 
-// Examples:
+Examples:
 
-// - `isWithinRange(4, { min: 0, max: 5 })` ➞ true
-// - `isWithinRange(4, { min: 4, max: 5 })` ➞ true
-// - `isWithinRange(4, { min: 6, max: 10 })` ➞ false
-// - `isWithinRange(5, { min: 5, max: 5 })` ➞ true
-//  */
+- `isWithinRange(4, { min: 0, max: 5 })` ➞ true
+- `isWithinRange(4, { min: 4, max: 5 })` ➞ true
+- `isWithinRange(4, { min: 6, max: 10 })` ➞ false
+- `isWithinRange(5, { min: 5, max: 5 })` ➞ true
+ */
 
-// console.log(`******01******`);
-// function isWithinRange(num, obj) {
-//   if (num >= obj.min && num <= obj.max) return console.log(true);
-//   else return console.log(false);
-// }
+console.log(`******01******`);
+function isWithinRange(num, obj) {
+  if (num >= obj.min && num <= obj.max) return console.log(true);
+  else return console.log(false);
+}
 
-// isWithinRange(4, { min: 0, max: 5 });
-// isWithinRange(4, { min: 4, max: 5 });
-// isWithinRange(4, { min: 6, max: 10 });
-// isWithinRange(5, { min: 5, max: 5 });
+isWithinRange(4, { min: 0, max: 5 });
+isWithinRange(4, { min: 4, max: 5 });
+isWithinRange(4, { min: 6, max: 10 });
+isWithinRange(5, { min: 5, max: 5 });
 
-// /*
-// **2. Scrabble.**
-// Create a function `calcMaxScrabbleScore`, given an array of scrabble tiles, counts the maximum score that a player can earn from the tiles in their hand.
-// Example:
-// ```javascript
-// const scrabbleHand = [
-//   { tile: "N", score: 1 },
-//   { tile: "K", score: 5 },
-//   { tile: "Z", score: 10 },
-//   { tile: "X", score: 8 },
-//   { tile: "D", score: 2 },
-//   { tile: "A", score: 1 },
-//   { tile: "E", score: 1 }
-// ]
-// ```
-// `console.log(calcMaxScrabbleScore(scrabbleHand));` ➞ 28
+/*
+**2. Scrabble.**
+Create a function `calcMaxScrabbleScore`, given an array of scrabble tiles, counts the maximum score that a player can earn from the tiles in their hand.
+Example:
+```javascript
+const scrabbleHand = [
+  { tile: "N", score: 1 },
+  { tile: "K", score: 5 },
+  { tile: "Z", score: 10 },
+  { tile: "X", score: 8 },
+  { tile: "D", score: 2 },
+  { tile: "A", score: 1 },
+  { tile: "E", score: 1 }
+]
+```
+`console.log(calcMaxScrabbleScore(scrabbleHand));` ➞ 28
 
-//     [
-//       { tile: "N", score: 1 },
-//       { tile: "K", score: 5 },
-//       { tile: "Z", score: 10 },
-//       { tile: "X", score: 8 },
-//       { tile: "D", score: 2 },
-//       { tile: "A", score: 1 },
-//       { tile: "E", score: 1 }
-//     ]
+    [
+      { tile: "N", score: 1 },
+      { tile: "K", score: 5 },
+      { tile: "Z", score: 10 },
+      { tile: "X", score: 8 },
+      { tile: "D", score: 2 },
+      { tile: "A", score: 1 },
+      { tile: "E", score: 1 }
+    ]
 
-// Here the player's maximum score would be `1 + 5 + 10 + 8 + 2 + 1 + 1` = 28
-//  */
+Here the player's maximum score would be `1 + 5 + 10 + 8 + 2 + 1 + 1` = 28
+ */
 
-// console.log(`******02******`);
+console.log(`******02******`);
 
-// const scrabbleHand = [
-//   { tile: "N", score: 1 },
-//   { tile: "K", score: 5 },
-//   { tile: "Z", score: 10 },
-//   { tile: "X", score: 8 },
-//   { tile: "D", score: 2 },
-//   { tile: "A", score: 1 },
-//   { tile: "E", score: 1 },
-// ];
+const scrabbleHand = [
+  { tile: "N", score: 1 },
+  { tile: "K", score: 5 },
+  { tile: "Z", score: 10 },
+  { tile: "X", score: 8 },
+  { tile: "D", score: 2 },
+  { tile: "A", score: 1 },
+  { tile: "E", score: 1 },
+];
 
-// function calcMaxScrabbleScore(arr) {
-//   let totalScore = 0;
+function calcMaxScrabbleScore(arr) {
+  let totalScore = 0;
 
-//   for (const score of arr) {
-//     totalScore += score["score"];
-//   }
-//   return totalScore;
-// }
-// console.log(calcMaxScrabbleScore(scrabbleHand));
+  for (const score of arr) {
+    totalScore += score["score"];
+    // totalScore += score.score;
+  }
+  return totalScore;
+}
+console.log(calcMaxScrabbleScore(scrabbleHand));
 
-// /*
-// **3. Is it an empty object?**
-// Create a function `isEmptyObject` that returns true if an object is empty, and false if otherwise.
+/*
+**3. Is it an empty object?**
+Create a function `isEmptyObject` that returns true if an object is empty, and false if otherwise.
 
-// Examples:
+Examples:
 
-// - `isEmptyObject({})` ➞ true
-// - `isEmptyObject({a: 1})` ➞ false
-//  */
+- `isEmptyObject({})` ➞ true
+- `isEmptyObject({a: 1})` ➞ false
+ */
 
-// console.log(`******03******`);
+console.log(`******03******`);
 
-// function isEmptyObject(obj) {
-//   if (Object.keys(obj).length === 0) console.log(true);
-//   else console.log(false);
-// }
+function isEmptyObject(obj) {
+  if (Object.keys(obj).length === 0) console.log(true);
+  else console.log(false);
+}
 
-// isEmptyObject({});
-// isEmptyObject({ a: 1 });
+isEmptyObject({});
+isEmptyObject({ a: 1 });
 
-// /*
-// **4. Counting Letters.**
-// Create a function that counts the number of occurrences of each letter in a string. Return an object with key value pairs of letters and the number of occurrences for each letter.
+/*
+**4. Counting Letters.**
+Create a function that counts the number of occurrences of each letter in a string. Return an object with key value pairs of letters and the number of occurrences for each letter.
 
-// Example:
+Example:
 
-// - `countLetters("tree")` ➞ {t: 1, r: 1, e: 2} */
+- `countLetters("tree")` ➞ {t: 1, r: 1, e: 2} */
 
-// console.log(`******04-method-1******`);
+console.log(`******04-method-1******`);
 
-// function countLetters(str) {
-//   const myString = str.split("");
+function countLetters(str) {
+  const myString = str.split("");
 
-//   let myResult = {};
+  let myResult = {};
 
-//   for (const el in myString) {
-//     if (myResult.hasOwnProperty([myString[el]])) {
-//       myResult[myString[el]]++;
-//     } else {
-//       myResult[myString[el]] = 1;
-//     }
-//   }
-//   return myResult;
-// }
+  for (const el in myString) {
+    if (myResult.hasOwnProperty([myString[el]])) {
+      myResult[myString[el]]++;
+    } else {
+      myResult[myString[el]] = 1;
+    }
+  }
+  return myResult;
+}
 
-// console.log(countLetters("tree"));
+console.log(countLetters("tree"));
 
-// console.log(`******04-method-2******`);
+console.log(`******04-method-2******`);
 
-// function countLetter(string) {
-//   let myResult = {};
-//   const myArray = string.split("");
-//   for (let i = 0; i < myArray.length; i++) {
-//     if (myResult[myArray[i]] === undefined) {
-//       myResult[myArray[i]] = 1;
-//     } else {
-//       myResult[myArray[i]]++;
-//     }
-//   }
-//   return myResult;
-// }
+function countLetter(string) {
+  let myResult = {};
+  const myArray = string.split("");
+  for (let i = 0; i < myArray.length; i++) {
+    if (myResult[myArray[i]] === undefined) {
+      myResult[myArray[i]] = 1;
+    } else {
+      myResult[myArray[i]]++;
+    }
+  }
+  return myResult;
+}
 
-// console.log(countLetter("tree"));
+console.log(countLetter("tree"));
 
-// /*
+/*
 
-// **5. Free Shipping.**
-// Create a function that determines whether an online order should get free shipping. An order gets free shipping if the total cost of items exceeds €50.
+**5. Free Shipping.**
+Create a function that determines whether an online order should get free shipping. An order gets free shipping if the total cost of items exceeds €50.
 
-// Examples:
+Examples:
 
-// - `freeShipping({ "Sponge": 3.50, "Soap": 5.99 })` ➞ false
-// - `freeShipping({ "Surround Sound Equipment": 499.99 })` ➞ true
-// - `freeShipping({ "Wool": 13.99, "Knitting Needles": 15.50, "Bag": 13.99 })` ➞ false */
+- `freeShipping({ "Sponge": 3.50, "Soap": 5.99 })` ➞ false
+- `freeShipping({ "Surround Sound Equipment": 499.99 })` ➞ true
+- `freeShipping({ "Wool": 13.99, "Knitting Needles": 15.50, "Bag": 13.99 })` ➞ false */
 
-// console.log(`******05******`);
+console.log(`******05******`);
 
-// function freeShipping(obj) {
-//   let sum = 0;
+function freeShipping(obj) {
+  let sum = 0;
 
-//   for (const val in obj) {
-//     sum += obj[val];
-//   }
+  for (const val in obj) {
+    sum += obj[val];
+  }
 
-//   if (sum > 50) {
-//     console.log(true);
-//   } else {
-//     console.log(false);
-//   }
-// }
+  if (sum > 50) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
+}
 
-// freeShipping({ Sponge: 3.5, Soap: 5.99 });
-// freeShipping({ "Surround Sound Equipment": 499.99 });
-// freeShipping({ Wool: 13.99, "Knitting Needles": 15.5, Bag: 13.99 });
+freeShipping({ Sponge: 3.5, Soap: 5.99 });
+freeShipping({ "Surround Sound Equipment": 499.99 });
+freeShipping({ Wool: 13.99, "Knitting Needles": 15.5, Bag: 13.99 });
 /* 
 **6. Programming Object.**
 
@@ -219,6 +220,8 @@ for (const values in programming) {
 }
 
 programming.worthwhile();
+
+Object.freeze(programming);
 
 // If we do not call a method (function), nothing will happen, no error and we get '[Function: worthwhile]'. we get function itself.
 
